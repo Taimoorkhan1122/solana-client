7b7c6b93024c13ca5590ec1f730ce9372b8d39f4
taimoorkhan1122 <khant8k83@gmail.com>
2022-12-10 15:44:26 +0500

    creating mint, token account and mint token

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { initializeKeypair } from "./initializeKeypair";
 import * as web3 from "@solana/web3.js";
 import * as token from "@solana/spl-token";
 
+// Initialize new mint
 async function createNewMint(
     connection: web3.Connection,
     payer: web3.Keypair,
@@ -23,15 +24,48 @@ async function createNewMint(
     return mint;
 }
 
-async function tokenMintAccount(
+// Create mint account
+async function tokenTokenAccount(
     connection: web3.Connection,
     payer: web3.Keypair,
     mint: web3.PublicKey,
     owner: web3.PublicKey,
-): Promise<web3.PublicKey> {
-    const tokenAccount = await token.createAccount(connection, payer, mint, owner);
+): Promise<token.Account> {
+    const tokenAccount = await token.getOrCreateAssociatedTokenAccount(connection, payer, mint, owner);
 
     return tokenAccount;
+}
+
+// Mint token
+async function mintToken(
+    connection: web3.Connection,
+    payer: web3.Keypair,
+    mint: web3.PublicKey,
+    destination: web3.PublicKey,
+    authority: web3.Keypair,
+    amount: number,
+) {
+    const mintInfo = await token.getMint(connection, mint);
+    console.log( "amount => ",amount * 10 ** mintInfo.decimals);
+    
+    const tnxSignature = await token.mintTo(
+        connection,
+        payer,
+        mint,
+        destination,
+        authority,
+        // whatever the amount of decimal is x that with 10^2
+        /*
+          so if we have 
+          amount = 1, decimals = 2  
+          then amount will be 100 and recipient will recieve 100 * 0.01 = 1 token
+        */ 
+        amount * 10 ** mintInfo.decimals,
+    );
+
+    console.log(
+        `Mint Token Transaction: https://explorer.solana.com/tx/${tnxSignature}?cluster=devnet`,
+    );
 }
 
 async function main() {
@@ -39,6 +73,14 @@ async function main() {
     const user = await initializeKeypair(connection);
 
     console.log("PublicKey:", user.publicKey.toBase58());
+
+    // decimals set to 2 = 0.01
+    // decimals set to 3 = 0.001
+    const mint = await createNewMint(connection, user, user.publicKey, user.publicKey, 2);
+
+    const tokenAccount = await tokenTokenAccount(connection, user, mint, user.publicKey);
+
+    await mintToken(connection, user, mint, tokenAccount.address, user, 100);
 }
 
 main()
